ad25abc09fbc2f41d246c6ec44e3125eafd23a07
kwaw <kwaw@kwaw-HP-625.(none)>
2013-07-24 08:34:45 +0000

    db reviced

---
+++ gurt/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from models import Grade, Grade_Commet, Institution, Person, Person_Comment
+
+
+
+
+
+admin.site.register(Institution)
+admin.site.register(Person)
+admin.site.register(Person_Comment)
+admin.site.register(Grade)
+admin.site.register(Grade_Commet)
+
--- gurt/models.py
+++ gurt/models.py
@@ -1,43 +1,103 @@
 from django.db import models
 
+
+
 class Institution(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
-        return self.id
+        return self.name
     
     def get_url(self):
-        return self.id
-
-class Person_Comment(models.Model):
-    comment = models.CharField(max_length=120,blank=True,null=True)
-    reated = models.DateTimeField(auto_now=True)
+        return '/gurt/institution/%s' % self.id
     
-    def __unicode__(self):
-        return self.id
+    class Meta:
+        db_table = 'institution'
+ 
     
-class Grade_Commet(models.Model):
-    comment = models.CharField(max_length=120,blank=True,null=True)
-    created = models.DateTimeField(auto_now=True)
-  
-    def __unicode__(self):
-        return self.id
+ 
+ 
     
 class Person(models.Model):
     name = models.CharField(max_length=50)
     department = models.CharField(max_length=20,blank=True)
-    grade = models.ForeignKey(Grade)
     institution = models.ForeignKey(Institution)
-    comment = models.ForeignKey(Person_Comment)
     
     def __unicode__(self):
-        return self.id
+        return self.name
     
     def get_url(self):
-        return self.id
+        return '/gurt/person/%s' % self.id
     
+    class Meta:
+        db_table = 'person'
+        
+        
+        
+
+        
+class Grade(models.Model):
+    GRADE = ((1,1),(2,2),(3,3),(4,4),(5,5))
+    person = models.ForeignKey(Person)
+    hotness = models.IntegerField(choices=GRADE)
+    easeness = models.IntegerField(choices=GRADE)
+    exam_diff = models.IntegerField(choices=GRADE) #exam_difficulty
+    helpfulness = models.IntegerField(choices=GRADE)
+    clearity = models.IntegerField(choices=GRADE)
+    kownledge = models.IntegerField(choices=GRADE)
+    strict = models.IntegerField(choices=GRADE)
+    
+    def __unicode__(self, ):
+        return str(self.id)
+    
+    class Meta:
+        db_table = 'grade'
+
+
+    
+class Person_Comment(models.Model):
+    person = models.ForeignKey(Person)
+    comment = models.CharField(max_length=120,blank=True,null=True)
+    reated = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.comment
+    
+    class Meta:
+        db_table = 'person_comment'
+        verbose_name = "Comments on Person"
+    
+    
+
+
+    
+class Grade_Commet(models.Model):
+    grade = models.ForeignKey(Grade)
+    comment = models.CharField(max_length=120,blank=True,null=True)
+    created = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.comment
+    
+    def get_url(self):
+        return '/gurt/gradecomment/%s' % self.id
+    
+    class Meta:
+        db_table = 'grade_comment'
+        verbose_name = "Comments on Grade"
+    
+ 
+ 
+ 
+
+    
+    
+
+    
+
+
     
     
     
